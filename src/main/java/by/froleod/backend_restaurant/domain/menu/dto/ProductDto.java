1880c47dd5e61cd620e15c9f39ea784a6f00dc2e
LFrolov <froleod@gmail.com>
2025-02-23 22:08:13 +0300

    bugfix: добавление айди к продукту

--- src/main/java/by/froleod/backend_restaurant/domain/menu/dto/ProductDto.java
+++ src/main/java/by/froleod/backend_restaurant/domain/menu/dto/ProductDto.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class ProductDto {
 
+    private Long id;
+
     private String name;
 
     private double price;
@@ -12,36 +14,4 @@ public class ProductDto {
     private String description;
 
     private String imageUrl;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
 }
